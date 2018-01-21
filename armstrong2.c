f117afb77159d250e5377d801dfa7b0e1dc3b2c6
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-01-21 09:51:36 +0530

    Create armstrong2.c

---
+++ armstrong2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int a, b, c, result = 0;
+ printf("Enter a three digit integer: ");
+    scanf("%d", &a);
+    b = a;
+    while (b != 0)
+    {
+        remainder = b%10;
+        result += c*c*c;
+        b/= 10;
+    }
+
+    if(result == a)
+        printf("%d is an Armstrong number.",a);
+    else
+        printf("%d is not an Armstrong number.",a);
+
+    return 0;
+}
