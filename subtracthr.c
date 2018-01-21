81c0a7a4793e6a07c23319e374583095cea73cd3
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-01-21 12:17:07 +0530

    Create subtracthr.c

---
+++ subtracthr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,y;
+printf("enter the hr:min of a");
+scanf("%d",&a);
+printf("enter the hr:min of b");
+scanf("%d",&b);
+y=sub(a-b);
+printf("ans is");
+return 0;
+}
