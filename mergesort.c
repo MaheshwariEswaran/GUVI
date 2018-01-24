ee7628666d72b80cb5bb049e2297f6f0b84387e1
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-01-24 11:15:52 +0000

    Create mergesort.c

---
+++ mergesort.c
@@ -0,0 +1,24 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int a[150],i,n=4,j,flag;
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<n;j++)
+{
+	if(a[i]>a[j])
+  {
+flag=a[i];
+	    a[i]=a[j];
+	   a[j]= flag;
+}
+}
+}
+	for(i=0;i<n;i++)
+  {
+  	for(i=0;i<n;i++)
+    return 0;
+    }
