83c9e573bf52479ffa14e2a431bb759f81490343
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-05 15:49:52 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+char a[100];
+int p,q;
+printf("\n enter the string");
+scanf("%s",&a);
+printf("\n enter the no. of times");
+scanf("%d",&p);
+for(q=0;q<p;q++)
+{
+	printf("\n %s",a);
+}
+	return 0;
+}
+
+  
