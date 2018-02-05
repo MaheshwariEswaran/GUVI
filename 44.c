a0a88480076811ed5749acec0102d09d7994236b
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-05 16:06:18 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int x;
+printf("\n enter the number");
+scanf("%d",&x);
+if(x>0&&x<=10)
+{
+	printf("\n YES");
+}
+else
+{
+	printf("\n NO");
+}
+	return 0;
+}
