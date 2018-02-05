6fa2ff7e3dfd240ee1fe2f1b88a7a639957269a9
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-05 16:49:52 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	int x,y,z;
+  clrscr();
+	printf("\n enter the two numbers");
+	scanf("%d %d",&x,&y);
+	z=x+y;
+	printf("\n addition value is %d",z);
+	return 0;
+}
