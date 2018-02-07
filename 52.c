8860d586c352b0c6baa2e8c605c34e763ec3024b
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-07 16:03:47 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s;
+printf("\n Enter the number");
+scanf("%d",&s);
+switch(s)
+{
+    case 1:
+    printf("\n  ONE");
+    break;
+    case 2:
+    printf("\n TWO");
+    break;
+    case 3:
+    printf("\n THREE"); 
+    break;
+    case 4:
+    printf("\n FOUR");
+    break;
+    case 5: 
+    printf("\n FIVE");
+    break;
+    case 6: 
+    printf("\n SIX");
+    break;
+    case 7:
+    printf("\n SEVEN");
+    break;
+    case 8:
+    printf("\n EIGHT"); 
+    break;
+    case 9:
+    printf("\n NINE");
+    break;
+    case 10:
+    printf("\n TEN");
+    break;
+    default:
+    printf("\n enter single digit number");
+    break;
+}
+}
