34a1456d46282033a407c9670276b86e768156f0
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-09 16:15:11 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int a[10],t,s,max;
+clrscr();
+printf("\n enter the 10 numbers:");
+for(t=0;t<10;t++)
+{
+	scanf("%d",&n[t]);
+}
+for(t=0;t<10;t++)
+{
+for(s=0;s<10;s++)
+{
+	if(a[s]>a[t])
+		max=a[s];
+}
+}
+printf("\n the maximum value is :%d ", max);
+	return 0;
+}
