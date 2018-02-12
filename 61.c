62021a77ea1a5d94d45b299e70e4daac351d3a75
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-12 16:14:22 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main(void)
+{
+  char y[10];
+  int x,t;
+  printf("\n enter the string");
+  scanf("%s",&y);
+  printf("\n enter the x value \n");
+  scanf("%d",&x);
+  for(t=0;t<x;t++)
+  {
+  printf("%c",y[i]);
+  }
+	return 0;
+}
