4178a79d178e0cd2ad7208e702a39b4896198d49
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-02-28 16:22:14 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+int x;
+float t;
+printf("\n Enter the number:");
+scanf("%f",&t);
+x=t;
+if(x>t)
+printf("\n %d",x);
+else
+printf("\n %d",x+1);
+	return 0;
+}
