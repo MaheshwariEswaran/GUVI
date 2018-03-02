88f732ee55d8fa40fad80f667aeb8cae62e208c3
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-03-02 15:50:29 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+	int y;
+	printf("\n Enter the number:");
+	scanf("%d",&y);
+		if(y%13==0)
+		printf("\n Multiples of 13");
+		else 
+		printf("\n Not a multiple of 13");
+	return 0;
+}
