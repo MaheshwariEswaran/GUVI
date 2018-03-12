243b35fa18542bdaa28606545ed54cb2aee0efae
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-03-12 16:22:00 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	float p,s,area;
+	printf("\n enter the length and breadth");
+	scanf("%5f %5f",&p,&s);
+	printf(" the area of farm is %.5f",p*s);
+	return 0;
+}
