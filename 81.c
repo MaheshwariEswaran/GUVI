fdeacd43184d52e5cd85549ae0342d695e9dc431
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-03-12 16:13:40 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+	int a,b,i;
+	printf("\n enter the num of ninjas in kabali and opposite group");
+	for(i=0;i<3;i++)
+	{
+	scanf("%d %d",&a,&b);
+		}
+	printf("\n the difference is");
+	if(b!=0)
+	{
+	printf("%d\n",b-a);
+	}
+		return 0;
+}
