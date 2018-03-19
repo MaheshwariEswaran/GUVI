cb0e8a50d3660fc64ef83949e1ea5cb108c0ee07
MaheshwariEswaran <35221114+MaheshwariEswaran@users.noreply.github.com>
2018-03-19 16:46:52 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h> 
+int main(void)
+{
+ int temp;
+ clrscr();
+ printf("\n Enter the temp in celsius");
+ scanf("%d",&temp);
+ printf("the kelvin temp is %d",temp+273);
+	return 0;
+}
